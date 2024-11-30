98dd6b17a21bc1c8276288eacc78489a5fe04e82
stinson122 <136882168+stinson122@users.noreply.github.com>
2024-11-30 18:21:05 +0800

    asm + c daxpy with c simd version

--- daxpy.asm
+++ daxpy.asm
@@ -3,13 +3,24 @@ bits 64
 default rel
 global daxpy
 
-; void daxpy(int n, double A, double *X, double *Y, double *Z)
 daxpy:
-    ;test only first (i=1)
-    movsd xmm1, [rsi] ;X[i] to xmm1
-    mulsd xmm1, xmm0 ; A * X[i]
-    addsd xmm1, [rdx] ; A * X[i] + Y[i]
-    movsd [rcx], xmm1 ;Z[i]
+    xor r10, r10
 
+L1:
+    ;call in C is extern void daxpy(int n, double A, double *X, double *Y, double *Z)
+    ; n@rcx A@xmm1 X@r9 Y@r10 Z@rdx
+    mov rax, r8
+    movsd xmm4, [rax + r10*8] ;X
+    mov rax, r9
+    movsd xmm5, [rax + r10*8] ;Y
+
+    mulsd xmm4, xmm1 ;A*X
+    addsd xmm4, xmm5 ;A*X + Y
+    mov rax, rdx
+    movsd [rax + r10*8], xmm4 ;Z
+    
+    inc r10
+    cmp r10, rcx
+    jl L1
     xor rax, rax
     ret
--- main.c
+++ main.c
@@ -1,23 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <immintrin.h>
 
+//asm function
 extern void daxpy(int n, double A, double *X, double *Y, double *Z);
 
+//C with SIMD instuctions
+void daxpy_C(int n, double A, double* X, double* Y, double* Z) {
+    
+    //__m256d - double, 256-bit
+    __m256d vectorA = _mm256_set1_pd(A); // set A to all vector elements
+    int i;
+    for (i = 0; i < n; i += 4) { //4 offset
+        __m256d vectorX = _mm256_loadu_pd(&X[i]);
+        __m256d vectorY = _mm256_loadu_pd(&Y[i]);
+        // (A*X) + Y
+        __m256d vectorZ = _mm256_add_pd(_mm256_mul_pd(vectorA, vectorX), vectorY);
+
+        _mm256_storeu_pd(&Z[i], vectorZ);
+    }
+}
+
 int main() {
-	//from example, added more to fit 10 elems
-    int n = 10;
-    double A = 2.0;
-    double X[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
-    double Y[] = {11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0};
-    double Z[10] = {0};
+    int n, i;
+    int sizes[] = {1 << 20};
+    double A, *X, *Y, *Z;
+    A = 2.0; //change
+    n = sizes[0];
+
+	//initialize X,Y,Z
+    X = (double *)malloc(n * sizeof(double));
+    Y = (double *)malloc(n * sizeof(double));
+    Z = (double *)malloc(n * sizeof(double));
+    for (i = 0; i < n; i++) {
+        X[i] = (double)(i+1);
+        Y[i] = (double)(i + 11);
+    }
 
     daxpy(n, A, X, Y, Z);
 
-	int i;
-    printf("Z: ");
-    for (i = 0; i < n; i++) {
-        printf("%.1f ", Z[i]);
+	//Print out 1st 10 elems of all vectors
+	printf("\nA: %.1lf", A);
+    printf("\nX: ");
+    for (i = 0; i < 10; i++) {
+        printf("%10.1lf ", X[i]);
     }
+    printf("\nY: ");
+    for (i = 0; i < 10; i++) {
+        printf("%10.1lf ", Y[i]);
+    }
+    printf("\nCompute DAXPY (A*X+Y):");
+    printf("\nZ:");
+    for (i = 0; i < 10; i++) {
+        printf("%10.1lf ", Z[i]);
+    }
+    
+    daxpy_C(n, A, X, Y, Z);
+    printf("\nDAXPY using SIMD instructions in C:");
+    printf("\nZ:");
+    for (i = 0; i < 10; i++) {
+        printf("%10.1lf ", Z[i]);
+    }
+    
     return 0;
 }
 
